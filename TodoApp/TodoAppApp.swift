dadbc86e32414acc0dab797edcb1518fdb9aed1a
Emami.114 <emami.114@outlook.de>
2023-08-01 18:49:59 +0200

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Ecc on 01.08.23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
